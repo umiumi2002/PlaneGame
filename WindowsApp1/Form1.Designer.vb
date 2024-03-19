215889ab1dcf446674a909d1b7ccc74d4ad2afd4
フルヤミウ <miyamiu19642002@gmail.com>
2024-03-19 18:02:40 +0900

    make basefield

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -23,12 +23,15 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.plane = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -44,20 +47,22 @@ Partial Class Form1
         'plane
         '
         Me.plane.AutoSize = True
+        Me.plane.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.plane.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.plane.Location = New System.Drawing.Point(167, 335)
+        Me.plane.Location = New System.Drawing.Point(148, 430)
         Me.plane.Name = "plane"
-        Me.plane.Size = New System.Drawing.Size(26, 18)
+        Me.plane.Size = New System.Drawing.Size(32, 22)
         Me.plane.TabIndex = 1
         Me.plane.Text = "●"
         '
         'Panel1
         '
         Me.Panel1.Controls.Add(Me.Button2)
-        Me.Panel1.Location = New System.Drawing.Point(591, 78)
+        Me.Panel1.Location = New System.Drawing.Point(1142, 18)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(200, 100)
+        Me.Panel1.Size = New System.Drawing.Size(333, 200)
         Me.Panel1.TabIndex = 2
+        Me.Panel1.Visible = False
         '
         'Button2
         '
@@ -68,18 +73,36 @@ Partial Class Form1
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'Timer1
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1000, 744)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1000, 540)
+        Me.ClientSize = New System.Drawing.Size(1000, 744)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.plane)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds
         Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -90,4 +113,5 @@ Partial Class Form1
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Button2 As Button
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -3,11 +3,14 @@
 
 Public Class Form1
     Public Property Angle As Integer
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.BackgroundImage = Image.FromFile("C:\Users\miyam\workspace\pygame\WindowsApp1\WindowsApp1\My Project\background.png") ' 画像のパスを指定してください
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        ' フォームのサイズをPictureBoxのサイズに合わせる
+        PictureBox1.Size = New Size(Me.ClientSize.Width, Me.ClientSize.Height)
+    End Sub
 
-        ' フォームのサイズを背景画像のサイズに合わせる
-        Me.ClientSize = New Size(Me.BackgroundImage.Width, Me.BackgroundImage.Height)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
     End Sub
 
 
@@ -25,6 +28,21 @@ Public Class Form1
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         plane.Top -= 3
 
+        If plane.Top <= 252 Then
+            Timer1.Enabled = False
+
+            Panel1.Visible = True
+
+
+
+            Dim newButton As New Button()
+            newButton.Text = "離陸許可"
+            newButton.Location = New Point(800, 40) ' ボタンの位置を設定してください
+            Me.Controls.Add(newButton)
+
+
+
+        End If
 
 
     End Sub
