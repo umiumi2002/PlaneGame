9381177a05349ce9032aea7b42b9f767ae87e1fd
フルヤミウ <miyamiu19642002@gmail.com>
2024-03-19 16:39:56 +0900

    practice1

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -22,29 +22,72 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.plane = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(125, 115)
+        Me.Button1.Location = New System.Drawing.Point(187, 119)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(94, 28)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "aaaaa"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'plane
+        '
+        Me.plane.AutoSize = True
+        Me.plane.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.plane.Location = New System.Drawing.Point(153, 271)
+        Me.plane.Name = "plane"
+        Me.plane.Size = New System.Drawing.Size(26, 18)
+        Me.plane.TabIndex = 1
+        Me.plane.Text = "●"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Location = New System.Drawing.Point(591, 78)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 100)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(29, 33)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1000, 540)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.plane)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents plane As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,3 +1,48 @@
-﻿Public Class Form1
+﻿Imports System.Reflection.Emit
+
+
+Public Class Form1
+    Public Property Angle As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackgroundImage = Image.FromFile("C:\Users\miyam\workspace\pygame\WindowsApp1\WindowsApp1\My Project\background.png") ' 画像のパスを指定してください
+
+        ' フォームのサイズを背景画像のサイズに合わせる
+        Me.ClientSize = New Size(Me.BackgroundImage.Width, Me.BackgroundImage.Height)
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MovePlaneUp()
+    End Sub
+
+    Private Sub MovePlaneUp()
+        ' label2を10ピクセル上に移動させる
+        Timer1.Enabled = True
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        plane.Top -= 3
+
+        If plane.Top <= 252 Then
+            Timer1.Enabled = False
+
+            Panel1.Visible = True
+
+
+
+            Dim newButton As New Button()
+            newButton.Text = "離陸許可"
+            newButton.Location = New Point(800, 40) ' ボタンの位置を設定してください
+            Me.Controls.Add(newButton)
+
+
+
+        End If
+
+
+    End Sub
+
 
 End Class
