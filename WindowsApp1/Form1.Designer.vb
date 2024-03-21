ef01338000ac075f49a594971e493adeeadb152b
フルヤミウ <miyamiu19642002@gmail.com>
2024-03-21 20:35:03 +0900

    DEP作成

--- WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/Form1.Designer.vb
@@ -24,67 +24,81 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.plane = New System.Windows.Forms.Label()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.plane1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Handoff_dep = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.takeoff = New System.Windows.Forms.Button()
+        Me.Hold_short_of_runway = New System.Windows.Forms.Button()
+        Me.Line_up_and_wait = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'Start
         '
-        Me.Button1.Location = New System.Drawing.Point(57, 54)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(70, 30)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Start"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Start.Location = New System.Drawing.Point(443, 356)
+        Me.Start.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(173, 68)
+        Me.Start.TabIndex = 0
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = True
         '
-        'plane
+        'plane1
         '
-        Me.plane.AutoSize = True
-        Me.plane.BackColor = System.Drawing.Color.Transparent
-        Me.plane.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.plane.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.plane.Location = New System.Drawing.Point(124, 531)
-        Me.plane.Margin = New System.Windows.Forms.Padding(0)
-        Me.plane.Name = "plane"
-        Me.plane.Size = New System.Drawing.Size(55, 15)
-        Me.plane.TabIndex = 1
-        Me.plane.Text = "● ANA"
+        Me.plane1.AutoSize = True
+        Me.plane1.BackColor = System.Drawing.Color.Transparent
+        Me.plane1.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.plane1.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.plane1.Location = New System.Drawing.Point(124, 531)
+        Me.plane1.Margin = New System.Windows.Forms.Padding(0)
+        Me.plane1.Name = "plane1"
+        Me.plane1.Size = New System.Drawing.Size(68, 15)
+        Me.plane1.TabIndex = 1
+        Me.plane1.Text = "● plane1"
         '
         'Panel1
         '
+        Me.Panel1.Controls.Add(Me.Handoff_dep)
         Me.Panel1.Controls.Add(Me.TextBox1)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Controls.Add(Me.Button2)
-        Me.Panel1.Controls.Add(Me.Button4)
-        Me.Panel1.Controls.Add(Me.Button3)
-        Me.Panel1.Location = New System.Drawing.Point(527, 28)
+        Me.Panel1.Controls.Add(Me.takeoff)
+        Me.Panel1.Controls.Add(Me.Hold_short_of_runway)
+        Me.Panel1.Controls.Add(Me.Line_up_and_wait)
+        Me.Panel1.Location = New System.Drawing.Point(523, 27)
         Me.Panel1.Margin = New System.Windows.Forms.Padding(2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(495, 344)
+        Me.Panel1.Size = New System.Drawing.Size(507, 347)
         Me.Panel1.TabIndex = 2
         Me.Panel1.Visible = False
+        '
+        'Handoff_dep
+        '
+        Me.Handoff_dep.Location = New System.Drawing.Point(171, 220)
+        Me.Handoff_dep.Name = "Handoff_dep"
+        Me.Handoff_dep.Size = New System.Drawing.Size(155, 50)
+        Me.Handoff_dep.TabIndex = 6
+        Me.Handoff_dep.Text = "DEPハンドオフ"
+        Me.Handoff_dep.UseVisualStyleBackColor = True
+        Me.Handoff_dep.Visible = False
         '
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.TextBox1.Location = New System.Drawing.Point(61, 47)
+        Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(357, 23)
+        Me.TextBox1.Size = New System.Drawing.Size(392, 117)
         Me.TextBox1.TabIndex = 5
         '
         'Label1
@@ -97,32 +111,35 @@ Partial Class Form1
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Label1"
         '
-        'Button2
+        'takeoff
         '
-        Me.Button2.Location = New System.Drawing.Point(171, 170)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(155, 48)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "離陸許可"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.takeoff.Location = New System.Drawing.Point(171, 170)
+        Me.takeoff.Name = "takeoff"
+        Me.takeoff.Size = New System.Drawing.Size(155, 48)
+        Me.takeoff.TabIndex = 3
+        Me.takeoff.Text = "離陸許可"
+        Me.takeoff.UseVisualStyleBackColor = True
+        Me.takeoff.Visible = False
         '
-        'Button4
+        'Hold_short_of_runway
         '
-        Me.Button4.Location = New System.Drawing.Point(171, 276)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(155, 46)
-        Me.Button4.TabIndex = 2
-        Me.Button4.Text = "滑走路前待機"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.Hold_short_of_runway.Location = New System.Drawing.Point(171, 276)
+        Me.Hold_short_of_runway.Name = "Hold_short_of_runway"
+        Me.Hold_short_of_runway.Size = New System.Drawing.Size(155, 46)
+        Me.Hold_short_of_runway.TabIndex = 2
+        Me.Hold_short_of_runway.Text = "滑走路前待機"
+        Me.Hold_short_of_runway.UseVisualStyleBackColor = True
+        Me.Hold_short_of_runway.Visible = False
         '
-        'Button3
+        'Line_up_and_wait
         '
-        Me.Button3.Location = New System.Drawing.Point(171, 224)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(155, 46)
-        Me.Button3.TabIndex = 1
-        Me.Button3.Text = "滑走路内待機"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.Line_up_and_wait.Location = New System.Drawing.Point(171, 224)
+        Me.Line_up_and_wait.Name = "Line_up_and_wait"
+        Me.Line_up_and_wait.Size = New System.Drawing.Size(155, 46)
+        Me.Line_up_and_wait.TabIndex = 1
+        Me.Line_up_and_wait.Text = "滑走路内待機"
+        Me.Line_up_and_wait.UseVisualStyleBackColor = True
+        Me.Line_up_and_wait.Visible = False
         '
         'Timer1
         '
@@ -156,7 +173,7 @@ Partial Class Form1
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.Label3.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Label3.Location = New System.Drawing.Point(310, 185)
+        Me.Label3.Location = New System.Drawing.Point(923, 393)
         Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(22, 15)
@@ -169,16 +186,29 @@ Partial Class Form1
         'Timer3
         '
         '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Green
+        Me.Label4.Font = New System.Drawing.Font("MS UI Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label4.Location = New System.Drawing.Point(625, 138)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(283, 64)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "ゲーム説明" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Startボタンを押すとゲーム開始" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "各planeを押すと場合に応じた指示が出せる" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1029, 666)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.plane)
+        Me.Controls.Add(Me.plane1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.Name = "Form1"
@@ -192,18 +222,20 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Button1 As Button
-    Friend WithEvents plane As Label
+    Friend WithEvents Start As Button
+    Friend WithEvents plane1 As Label
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Timer1 As Timer
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents takeoff As Button
+    Friend WithEvents Hold_short_of_runway As Button
+    Friend WithEvents Line_up_and_wait As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Timer2 As Timer
     Friend WithEvents Timer3 As Timer
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Handoff_dep As Button
+    Friend WithEvents Label4 As Label
 End Class
--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -6,76 +6,121 @@ Public Class Form1
     Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
         ' フォームのサイズをPictureBoxのサイズに合わせる
         PictureBox1.Size = New Size(Me.ClientSize.Width, Me.ClientSize.Height)
-        plane.BackColor = Color.Transparent
+        plane1.BackColor = Color.Transparent
 
     End Sub
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        plane1.BackColor = Color.Transparent
+        If Not Start.Enabled Then
+            Return
+        End If
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Start.Click
+        ' ボタンが押されていない場合は何もしない
+
+
+        ' ボタンが押されたら他の機能を有効にする
         Timer1.Enabled = True
+        Start.Hide()
     End Sub
 
     Private Sub MovePlaneUp()
-        plane.Top -= 1
+        plane1.Top -= 1
     End Sub
     Private Sub MovePlaneRight()
-        plane.Left += 1
+        Timer2.Interval = 100
+        plane1.Left += 1
     End Sub
     Private Sub MovePlaneRunway()
-        Timer2.Interval = 30
+        Timer2.Interval = 50
         ' plane.Left += 10
     End Sub
     Private Sub MovePlaneTakeOff()
-        Timer2.Interval = 20
+        Timer2.Interval = 10
         ' plane.Left += 20
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         ' planeを上に移動する
-        If plane.Top > 430 Then
+        If plane1.Top > 430 Then
             ' planeが190より上にある場合、上に移動
             MovePlaneUp()
         End If
 
         ' planeが252に達した場合、一時停止
-        If plane.Top < 495 Then
+        If plane1.Top < 495 Then
             Timer1.Enabled = False
-            Panel1.Visible = True
-            Label1.Text = plane.Name
-            TextBox1.Text = "ready to takeoff"
+            takeoff.Show()
+            Line_up_and_wait.Show()
+            Hold_short_of_runway.Show()
 
         End If
     End Sub
 
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
-        If plane.Top <= 430 Then
+        If plane1.Top <= 430 Then
             ' planeが190以下かつ0より上にある場合、右に移動
             MovePlaneRight()
         Else
             MovePlaneUp()
         End If
-        If plane.Left >= 250 Then
+        If plane1.Left >= 250 AndAlso plane1.Top >= 430 Then
             MovePlaneRunway()
 
-        ElseIf plane.Left >= 420 Then
+        ElseIf plane1.Left >= 420 AndAlso plane1.Top >= 430 Then
             MovePlaneTakeOff()
         End If
-        If plane.Left >= 960 Then
-            plane.Location = New Point(310, 185)
+        If plane1.Left >= 960 Then
+            plane1.Location = New Point(310, 185)
+            takeoff.Hide()
+            Line_up_and_wait.Hide()
+            Hold_short_of_runway.Hide()
+            Handoff_dep.Show()
+            TextBox1.AppendText("TWR >>" & plane1.Name & " ,contact to DEP " & Environment.NewLine)
             MovePlaneRight()
+        End If
+        If plane1.Left >= 960 AndAlso plane1.Top <= 390 Then
+            plane1.Hide()
+
         End If
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles takeoff.Click
         Timer2.Enabled = True
+        TextBox1.AppendText("TWR >>RWY 34, cleared to takeoff " & Environment.NewLine)
     End Sub
 
     Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
         MovePlaneTakeOff()
     End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Hold_short_of_runway.Click
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Handoff_dep.Click
+        TextBox1.AppendText(plane1.Name & " >> contact DEP " & Environment.NewLine)
+
+    End Sub
+
+    Private Sub plane1_Click(sender As Object, e As EventArgs) Handles plane1.Click
+        If Not Timer1.Enabled Then
+            Return
+        End If
+        If Panel1.Visible Then
+            ' パネルが表示されている場合、非表示にする
+            Panel1.Hide()
+        Else
+            ' パネルが非表示の場合、表示する
+            Panel1.Show()
+            Label1.Text = plane1.Name
+        End If
+
+    End Sub
+
 
 End Class
